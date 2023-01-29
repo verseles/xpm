55f16685f04793790675bdc6a274835a0c80c477
Hélio <insign@gmail.com>
2023-01-29 14:32:00 -0300

    removed onError, no needed

--- lib/os/downloader/downloader.dart
+++ lib/os/downloader/downloader.dart
@@ -3,10 +3,10 @@ import 'dart:io';
 import 'package:xpm/os/downloader/downloader_adapter.dart';
 
 class Downloader {
-  static String userAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36';
+  static String userAgent =
+      'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36';
 
   Function(double, int)? onProgress;
-  Function(Error)? onError;
   DownloaderAdapter adapter;
 
   Downloader(this.adapter);
@@ -17,25 +17,20 @@ class Downloader {
       int? segments,
       Function(Map<String, dynamic>)? onProgress,
       Function(Error)? onError}) async {
-
     if (!adapter.isAvailable) {
-      throw Exception('Downloader adapter ${adapter.executable.cmd} not available');
+      throw Exception(
+          'Downloader adapter ${adapter.executable.cmd} not available');
     }
 
-    segments = (segments ?? 1).abs(); 
-
+    segments = (segments ?? 1).abs();
 
     if (url.isEmpty) {
       throw Exception('URL not provided');
     }
-    try {
-      await adapter.download(url: url, destination: destination, segments: segments, onProgress: onProgress);
-    } on Error catch (e) {
-      if (onError != null) {
-        onError(e);
-      } else {
-        rethrow;
-      }
-    }
+    await adapter.download(
+        url: url,
+        destination: destination,
+        segments: segments,
+        onProgress: onProgress);
   }
 }
