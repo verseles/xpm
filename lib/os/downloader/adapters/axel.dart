6807dd42f3c9fb5c0f522be04f4a7fb31e424c67
Hélio <insign@gmail.com>
2023-01-29 14:15:02 -0300

    WIP: Axel Adapter

--- lib/os/downloader/adapters/axel.dart
+++ lib/os/downloader/adapters/axel.dart
@@ -24,12 +24,17 @@ class AxelAdapter implements DownloaderAdapter {
       int? segments,
       Function(Map<String, dynamic>)? onProgress}) async {
     executablePath = (await executable.find())!;
-    var args = [url, '-o', destination.path];
-    if (segments != null) {
-      args.insert(1, '-n');
-      args.insert(2, segments.toString());
+
+    if (destination.existsSync()) {
+      destination.deleteSync();
     }
-    Process.start(executablePath, args).then((Process process) {
+
+    Process.start(executablePath, [
+      url,
+      '--num-connections=$segments',
+      '--output=${destination.path}',
+      '--percentage'
+    ]).then((Process process) {
       process.stdout.transform(utf8.decoder).listen((data) {
         final lines = data.split('\n');
         for (final line in lines) {
@@ -43,20 +48,8 @@ class AxelAdapter implements DownloaderAdapter {
 
   Map<String, dynamic> parseAxelProgress(String progressString) {
     final Map<String, String> progress = {};
-    final match = RegExp(
-            r'(\d+)%\s+\|\s+(\d+\.\d\w)\s+/\s+(\d+\.\d\w)\s+\|\s+(\d+\.\d\w/s)\s+\|\s+(\d+\.\d\w/s)\s+\|\s+(\d+\.\d\w)\s+\|')
-        .firstMatch(progressString);
-    if (match != null && match.groupCount == 7) {
-      progress["percentComplete"] = match.group(1)!;
-      progress["downloaded"] = match.group(2)!;
-      progress["totalSize"] = match.group(3)!;
-      progress["speed"] = match.group(4)!;
-      progress["averageSpeed"] = match.group(5)!;
-      progress["eta"] = match.group(6)!;
-      progress["time"] = match.group(7)!;
-    } else {
-      print('NO MATCH: $progressString');
-
+    if (int.tryParse(progressString) != null) {
+      progress["percentComplete"] = progressString;
     }
     return progress;
   }
